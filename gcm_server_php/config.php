a7356d8d5d47cc556fc2f3163f022cf8655e6504
WenGV <51245738+WenGV@users.noreply.github.com>
2019-05-31 14:04:16 -0600

    Intial commit

---
+++ gcm_server_php/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "gcm");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyBnwcwTuI2J3JjTYJAtivzuUocUFtR3bKg"); // Place your Google API Key
+//define("androidGCMappKey", "AIzaSyDTkVxrfj520hfUSN_abdaB7JenyrCnnq8");
+?>
